b6b2ab9430f84e3c6efb4fdc27792d9e68ab43ed
DanDeanDean <dpd5518@psu.edu>
2025-08-31 09:35:12 -0400

    add div (centered)

--- home-page/src/main.ts
+++ home-page/src/main.ts
@@ -2,9 +2,13 @@ import './style.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Welcome to my Site!</h1>
+    <h1>Dan Dean, Dean</h1>
     <div class="card">
-      <p>Hello World</p>
+      <p>
+        Welcome to my site!
+        I am currently working as a DevOps engineer building robust backends.
+        Don't be alarmed if you find me tinkering on random things.
+      </p>
     </div>
   </div>
 `
