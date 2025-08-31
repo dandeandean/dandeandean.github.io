c001561daf41141a9a20875cbc2c46aa930344f3
DanDeanDean <dpd5518@psu.edu>
2025-08-31 13:07:02 -0400

    do some css

--- home-page/src/main.ts
+++ home-page/src/main.ts
@@ -1,19 +1,24 @@
 import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+function renderHTML() {
+  if (document.querySelector<HTMLDivElement>('#app') === null) {
+  }
+  document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <div class="header">
       <h1>
-        Welcome to my site!
+        Hello World!
       </h1>
     </div>
     <div class="card">
       <p>
-        Hey there, thanks for stopping by!
+        Welcom to my site, thanks for stopping by!
         I'm a DevOps engineer focused on building powerful back-ends.
         I'm also passionate about side projects, so you'll see a mix of professional work and personal experiments here.
       </p>
     </div>
   </div>
 `
+}
 
+renderHTML()
