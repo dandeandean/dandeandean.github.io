83965dfb54c4ef230d2ab5d4ce06afc27800912d
dandeandean <dandean44523@gmail.com>
2026-01-11 14:26:17 +0000

    Add new arch
    Add aarch64-linux for the asahi box

--- home-page/flake.nix
+++ home-page/flake.nix
@@ -1,33 +1,16 @@
 {
   description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
-  outputs =
-    { self, nixpkgs }@inputs:
+  outputs = { self, nixpkgs }@inputs:
     let
       # Supported systems for your flake packages, shell, etc.
-      systems = [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ];
+      systems = [ "x86_64-linux" "aarch64-darwin" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              zola
-            ];
-          };
-        }
-      );
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in { default = pkgs.mkShell { packages = with pkgs; [ zola ]; }; });
     };
 }
